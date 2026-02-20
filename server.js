4bb1577c7b08184ae3f1b500359c7985b1eb6165
John <faysal.heshmatullah@gmail.com>
2026-02-19 22:22:23 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js (directory: root)
+const express = require('express');
+const bodyParser = require('body-parser');
+const handler = require('./platorelay');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.all('*', (req, res) => {
+  handler(req, res);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
